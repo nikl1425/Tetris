8c76dc748ca5f79fddc57ad862c46e3db4bf73c2
Niklas Hjort <niklas@astne.dk>
2021-02-17 19:56:49 +0100

    OOP tort works

---
+++ game.py
@@ -0,0 +1,16 @@
+import pygame
+
+
+class Game:
+    def __init__(self, screen, width, height):
+        self.state = "intro"
+        self.screen = screen
+        self.width = width
+        self.height = height
+        self.background = pygame.draw.rect(screen, "black", (0, 0, self.width, self.height))
+
+    def render(self):
+        pass
+
+    def update(self):
+        pass

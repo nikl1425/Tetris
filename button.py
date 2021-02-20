a312b39be863f11153e7d1b43c5026a7984c3abe
Niklas Hjort <niklas@astne.dk>
2021-02-20 18:19:10 +0100

    button.py has text on it

--- button.py
+++ button.py
@@ -1,7 +1,12 @@
 import pygame
 
+
 class Button:
-    def __init__(self, x, y, width, height, color, hover_color):
+    pygame.init()
+    font = pygame.font.SysFont('arial', 25)
+    black = (0, 0, 0)
+
+    def __init__(self, x, y, width, height, color, hover_color, text):
         self.x = x
         self.y = y
         self.width = width
@@ -9,6 +14,7 @@ class Button:
         self.size = (x, y, width, height)
         self.color = color
         self.hover_color = hover_color
+        self.text = text
 
 
     def draw(self, window):
@@ -16,7 +22,7 @@ class Button:
         pygame.draw.rect(window, self.color, self.size)
         if self.x < mouse_PosX < (self.x + self.width) and self.y < mouse_PosY < (self.y + self.height):
             pygame.draw.rect(window, self.hover_color, self.size)
-
+        window.blit(self.font.render(self.text, True, self.black), (self.x, self.y))
 
     def check_hover(self):
         mouse_PosX, mouse_PosY = pygame.mouse.get_pos()
@@ -26,7 +32,7 @@ class Button:
         return False
 
 
-
+# print(pygame.font.get_fonts())
 
 
 
--- intro.py
+++ intro.py
@@ -1,6 +1,8 @@
 import pygame
 from button import Button
 
+light_grey = (209, 207, 203)
+dark_grey = (105, 104, 102)
 
 class intro:
     def __init__(self, screen, width, height):
@@ -10,7 +12,7 @@ class intro:
         self.width = width
         self.height = height
         self.background = pygame.image.load("assets/intro_background.png")
-        self.start_game = Button(200, 200, 50, 20, "red", "blue")
+        self.start_game = Button(width/2 - 25, height/2 + 30, 60, 30, light_grey, dark_grey, "Play!")
 
     def event_handler(self, event):
         mouseX, mouseY = pygame.mouse.get_pos()

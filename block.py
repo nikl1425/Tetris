9f39cea0af1324b2a1486a252f1ba566ffd16fd6
Niklas Hjort <niklas@astne.dk>
2021-02-28 17:43:45 +0100

    added color list

--- block.py
+++ block.py
@@ -9,15 +9,44 @@ def split_list(a_list):
     half = len(a_list)//2
     return a_list[:half], a_list[half:]
 
+
 class Block:
 
     L = ['.#..',
          '.#..',
-         '.##.']
+         '.##.',
+         '....']
+    T = ['....',
+         '###.',
+         '.#..',
+         '....']
+    I = ['#...',
+         '#...',
+         '#...',
+         '#...']
+    O = ['....',
+         '.##.',
+         '.##.',
+         '....']
+    S = ['....',
+         '..##',
+         '.##.',
+         '....']
+
+    # Colors
+    black = (0, 0, 0)
+    red = (255, 0, 0)
+    blue = (0, 0, 255)
+    green = (51, 255, 51)
+    pink = (204, 0, 204)
+    yellow = (255, 255, 0)
+
+    shapes = [L, T, I, O, S]
+    shape_colors = [red, blue, green, pink, yellow]
 
     def __init__(self, size):
         self.size = size
-        self.color = 0
+        self.color = (0, 0, 0)
         self.rotation = 0
         self.x = []
         self.y = []
@@ -27,7 +56,7 @@ class Block:
 
     def convert_shape_format(self):
         positions = []
-        format = self.L
+        format = self.I
 
         for i, line in enumerate(format):
             row = list(line)

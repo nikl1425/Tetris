0df26297f7378b74ccec08c79de230cb6acca92f
Niklas Hjort <niklas@astne.dk>
2021-03-07 10:54:03 +0100

    rotation works

--- block.py
+++ block.py
@@ -1,7 +1,7 @@
 import pygame
 from collections import defaultdict
 from random import randrange
-
+from operator import add
 
 def findOccurrences(s, ch):
     return [i for i, letter in enumerate(s) if letter == ch]
@@ -10,30 +10,60 @@ def split_list(a_list):
     half = len(a_list)//2
     return a_list[:half], a_list[half:]
 
+L = [['.#..',
+      '.#..',
+      '.##.',
+      '....'],
+     ['....',
+      '###.',
+      '#...',
+      '....'],
+     ['.##.',
+      '..#.',
+      '..#.',
+      '....'],
+     ['....',
+      '...#',
+      '.###',
+      '....']]
+T = [['....',
+      '###.',
+      '.#..',
+      '....'],
+     ['.#..',
+      '##..',
+      '.#..',
+      '....'],
+     ['....',
+      '.#..',
+      '###.',
+      '....'],
+     ['.#..',
+      '.##.',
+      '.#..',
+      '....']]
+I = [['#...',
+      '#...',
+      '#...',
+      '#...'],
+     ['....',
+      '####',
+      '....',
+      '....']]
+O = [['....',
+      '.##.',
+      '.##.',
+      '....']]
+S = [['....',
+      '..##',
+      '.##.',
+      '....'],
+     ['.#..',
+      '.##.',
+      '..#.',
+      '....']]
 
 class Block:
-
-    L = ['.#..',
-         '.#..',
-         '.##.',
-         '....']
-    T = ['....',
-         '###.',
-         '.#..',
-         '....']
-    I = ['#...',
-         '#...',
-         '#...',
-         '#...']
-    O = ['....',
-         '.##.',
-         '.##.',
-         '....']
-    S = ['....',
-         '..##',
-         '.##.',
-         '....']
-
     # Colors
     black = (0, 0, 0)
     red = (255, 0, 0)
@@ -47,41 +77,68 @@ class Block:
 
     def __init__(self, size):
         self.size = size # make this as tuple
-        self.color = self.black
         self.rotation = 0
+        self.currentblock = []
+        self.pos = []
+        self.shape = []
 
 
     def convert_shape_format(self, form):
         positions = []
+        formats = form[self.rotation % len(form)]
 
-        for i, line in enumerate(form):
+        for i, line in enumerate(formats):
             row = list(line)
             for j, column in enumerate(row):
                 if column == '#':
                     positions.append(list((j, i)))
-                    #print(positions)
-
+                    self.pos = positions
         return positions
 
     def next_block(self):
         random_int = randrange(0, 5)
         picked_shape = self.shapes[random_int]
         index = self.shapes.index(picked_shape)
+        self.shape = self.shapes[random_int]
         picked_color = self.shape_colors[index]
         return picked_color, picked_shape
 
     def create_block(self):
-        block_color, current_block = self.next_block()
-        current_block = self.convert_shape_format(current_block)
-        #print(current_block)
-
+        block_color, self.current_block = self.next_block()
+        current_block = self.convert_shape_format(self.shape)
         return current_block, block_color
 
+    def rotate_block(self, shape):
+        print("current:  " + str(shape))
+        new_shape = self.convert_shape_format(self.current_block)
+        print("new:  " + str(new_shape))
+        x_values_in_shape = [x for (x,_) in (l for l in shape)]
+        y_values_in_shape = [y for (_, y) in (i for i in shape)]
+        x_low = self.my_min(x_values_in_shape)
+        y_low = self.my_min(y_values_in_shape)
+        new_list = []
+        for element in new_shape:
+            x, y = element
+            x = x + x_low
+            y = y + y_low
+            new_list.append([x,y])
+        return new_list
+
+    def my_min(self, sequence):
+        """return the minimum element of sequence"""
+        low = sequence[0]  # need to start with some value
+        for i in sequence:
+            if i < low:
+                low = i
+        return low
 
 
 
 
-blob = Block(29)
 
-blob.create_block()
+
+
+
+
+
 
--- game.py
+++ game.py
@@ -2,6 +2,8 @@ import pygame
 from block import Block
 import time
 from collections import deque
+import numpy as np
+from copy import deepcopy
 
 gameX = 100
 gameY = 0
@@ -9,11 +11,12 @@ gameWidth = 400
 gameHeight = 400
 grid_width = 10
 grid_height = 20
-tetro = Block(20)
+
 clock = pygame.time.Clock()
 locked_positon = []
 grid_start = gameWidth // 3
-locked_list =[]
+locked_list = []
+tetro2 = Block(20)
 
 
 def iterate_through_locked_position(locked_pos_list):
@@ -22,6 +25,15 @@ def iterate_through_locked_position(locked_pos_list):
         for i in rect:
             locked_list.append(i)
             print("HEJ HEJ" + str(locked_list))
+
+
+class tetromino(object):
+    def __init__(self, row, column, color):
+        self.row = row
+        self.column = column
+        self.color = color
+        self.rotation = 0
+
 
 
 class Game:
@@ -35,13 +47,15 @@ class Game:
         self.game_window = pygame.Rect(gameX, gameY, gameWidth, gameHeight)
         self.background = pygame.image.load("assets/game_background.png")
         self.grid = []
+        self.tetro = Block(20)
         self.tetro_list = []
         self.locked_positions = deque([])
         self.locked_tetro = []
         self.blocks = Block(20)
-        self.current_tetro, self.current_tetro_color = tetro.create_block()
+        self.current_tetro, self.current_tetro_color = self.tetro.create_block()
         self.locked_tetro_list_rect = []
         self.locked_tetro_colors = []
+
 
     def event_handler(self, event):
         if event.type == pygame.KEYDOWN:
@@ -57,7 +71,9 @@ class Game:
             if event.key == pygame.K_SPACE:
                 for element in self.current_tetro:
                     element[1] += 15
-
+            if event.key == pygame.K_r:
+                self.tetro.rotation += 1
+                self.current_tetro = self.tetro.rotate_block(self.current_tetro)
 
     def render(self):
         self.background = pygame.transform.scale(self.background, (self.width, self.height))
@@ -79,16 +95,17 @@ class Game:
     def create_grid(self):
         for y in range(grid_height):
             for x in range(grid_width):
-                rect = pygame.Rect((x*self.blocks.size) + grid_start, y*self.blocks.size, self.blocks.size, self.blocks.size)
+                rect = pygame.Rect((x * self.blocks.size) + grid_start, y * self.blocks.size, self.blocks.size,
+                                   self.blocks.size)
                 self.grid.append(rect)
 
     def create_tetros(self):
         self.tetro_list = []
         for element in self.current_tetro:
             rect = pygame.Rect(element[0] * self.blocks.size + grid_start,
-                              element[1] * self.blocks.size,
-                              self.blocks.size,
-                              self.blocks.size)
+                               element[1] * self.blocks.size,
+                               self.blocks.size,
+                               self.blocks.size)
             self.tetro_list.append(rect)
 
     def create_locked_tetros(self):
@@ -96,17 +113,16 @@ class Game:
             rect_list, color = element
             for elements in rect_list:
                 rect = pygame.Rect(elements[0] * self.blocks.size + grid_start,
-                            elements[1] * self.blocks.size,
-                            self.blocks.size,
-                            self.blocks.size)
+                                   elements[1] * self.blocks.size,
+                                   self.blocks.size,
+                                   self.blocks.size)
                 this_set = (rect, color)
                 self.locked_tetro_list_rect.append(this_set)
                 self.locked_tetro_colors.append(color)
 
-
     def tetro_movement(self):
         hit_bottom = False
-        if any(element >= 19 for (_, element) in self.current_tetro) :
+        if any(element >= 19 for (_, element) in self.current_tetro):
             hit_bottom = True
         else:
             for element in self.current_tetro:
@@ -118,15 +134,20 @@ class Game:
             for rect in rect_list:
                 locked_rects.append(rect)
 
-        print(locked_rects)
+        locked_element = []
+        locked_element.extend([rects for (rects, _) in self.locked_tetro])
+        print(self.tetro.shape)
+        for i in locked_element:
+            for inner_element in i:
+
+                if any(rects[1] == inner_element[1] - 1 for rects in self.current_tetro) and any(
+                        rects[0] == inner_element[0] for rects in self.current_tetro):
+                    hit_bottom = True
 
         if hit_bottom:
             locked_tetro_set = (self.current_tetro, self.current_tetro_color)
             self.locked_tetro.append(locked_tetro_set)
-            self.current_tetro, self.current_tetro_color = tetro.create_block()
-        #print("current tetro: " + str(self.current_tetro))
-        #print("locked" + str(self.locked_tetro))
-        #print("locked_rect" + str(self.locked_tetro_list_rect))
+            self.current_tetro, self.current_tetro_color = self.tetro.create_block()
 
     def update(self):
         global next_tetro
@@ -134,13 +155,6 @@ class Game:
         self.tetro_movement()
         self.create_locked_tetros()
         self.create_tetros()
-        print(self.locked_tetro_colors)
-
-        #print('tetrolist ' + str(self.tetro_list))
-        #print(self.current_tetro)
-        #print(self.locked_positions)
-        #print('gridlist ' + str(self.grid))
-       #print("locked rect:   " + str(self.locked_rect))
 
     def main(self):
         clock.tick(1)
